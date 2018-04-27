dea8a5e56f89406c913789050eea2dac5f109dc3
Alkxyly <alkxyly@hotmail.com>
2018-04-27 08:24:31 -0300

    commit

--- app/src/main/java/aktecnologia/br/com/oraculo/PrincipalActivity.kt
+++ app/src/main/java/aktecnologia/br/com/oraculo/PrincipalActivity.kt
@@ -2,11 +2,20 @@ package aktecnologia.br.com.oraculo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_principal.*
+import org.jetbrains.anko.toast
 
 class PrincipalActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+
+        menu_personagens.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                toast("Clicou em personagens")
+            }
+        })
     }
 }
